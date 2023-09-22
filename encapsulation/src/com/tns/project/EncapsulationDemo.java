62f023bde086a2fa8ae4be1003f4788938276c02
acer <acer@TMP249-G2-M.bbrouter>
2023-09-22 14:04:10 +0530

    first commit

---
+++ encapsulation/src/com/tns/project/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tns.project;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulation ob=new Encapsulation();
+		//accessing the data members using dot operator
+		ob.serialno=33;
+		ob.name="surya";
+		ob.age=32;
+		ob.show();
+	}
+
+}
