61718326d851720b22fa6824aa9930be7c649b95
acer <acer@10.0.0.153>
2023-10-09 17:16:18 +0530

    COMMIT

---
+++ Projectss/src/com/tns/minterface/Demoo.java
@@ -0,0 +1,12 @@
+package com.tns.minterface;
+
+public class Demoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Student1 s=new Student1(1,"hema");
+if(s instanceof Registerable )
+	
+	}
+
+}
