de670bd6680d70af0c1e806b985e88a58c875402
acer <acer@192.168.1.41>
2023-10-18 15:54:38 +0530

    first commit

---
+++ Projectss/src/com/tns/Wrapperclass/WrapperDemo.java
@@ -0,0 +1,14 @@
+package com.tns.Wrapperclass;
+
+public class WrapperDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=15;
+Integer i=new Integer(x);
+System.out.println("x is object of integer type"+i);
+int u=i.intValue();
+System.out.println("u is primitive intger"+u);
+	}
+
+}
